4db61de0b9baf5727b2df9eb51de606b7ca05ad5
Roman <tajmur@gmail.com>
2024-04-03 19:02:45 +0700

    This is our first commit

---
+++ project_one/script.js
@@ -0,0 +1,36 @@
+// const person = {
+//       name: 'romanSherman',
+//       age: 34,
+//       isMarried: false
+// } ;
+
+// let arr = ['roma', 'lazio', 'milan'] ;
+
+// const obj = {
+//     name: 'Alertio',
+//     surname: 'Berritini',
+//     age: 48
+// } ;
+
+
+// let jerkOff = 'Valeron' ;
+// console.log(typeof(parseInt(jerkOff))) ;
+
+// let shipName = null ;
+
+// if(shipName) { 
+//   console.log ('Верный результат')
+// } else {
+//   console.log('Очевидный результат')
+// } ;
+
+// let answer = prompt('Are you 18 age?', 'Укажите ваш возраст');
+
+// console.log(typeof(null)) ;
+ 
+let romanK = true,
+    laraR = true,
+    chemiZ = true,
+    molly = false;
+
+    console.log(romanK || laraR || chemiZ || molly) ;
