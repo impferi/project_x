424f7e1ad66fab5b920ce8a88efe04a43a177b44
Roman <tajmur@gmail.com>
2024-04-04 13:47:22 +0700

    04.04 morning commit

--- project_one/script.js
+++ project_one/script.js
@@ -1,32 +1,32 @@
-// const person = {
-//       name: 'romanSherman',
-//       age: 34,
-//       isMarried: false
-// } ;
+const person = {
+      name: 'romanSherman',
+      age: 34,
+      isMarried: false
+} ;
 
-// let arr = ['roma', 'lazio', 'milan'] ;
+let arr = ['roma', 'lazio', 'milan'] ;
 
-// const obj = {
-//     name: 'Alertio',
-//     surname: 'Berritini',
-//     age: 48
-// } ;
+const obj = {
+    name: 'Alertio',
+    surname: 'Berritini',
+    age: 48
+} ;
 
 
-// let jerkOff = 'Valeron' ;
-// console.log(typeof(parseInt(jerkOff))) ;
+let jerkOff = 'Valeron' ;
+console.log(typeof(parseInt(jerkOff))) ;
 
-// let shipName = null ;
+let shipName = null ;
 
-// if(shipName) { 
-//   console.log ('Верный результат')
-// } else {
-//   console.log('Очевидный результат')
-// } ;
+if(shipName) { 
+  console.log ('Верный результат')
+} else {
+  console.log('Очевидный результат')
+} ;
 
-// let answer = prompt('Are you 18 age?', 'Укажите ваш возраст');
+let answer = prompt('Are you 18 age?', 'Укажите ваш возраст');
 
-// console.log(typeof(null)) ;
+console.log(typeof(null)) ;
  
 let romanK = true,
     laraR = true,
