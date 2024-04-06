b59826d03c68dfa9d1185414c355a8c88995eda0
Roman <tajmur@gmail.com>
2024-04-06 22:29:39 +0700

    Функции

--- project_one/script.js
+++ project_one/script.js
@@ -90,41 +90,96 @@
 //   console.log(i)
 // }
 
-let money = +prompt('Ваш бюджет на месяц?', ''),
-    time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
-let appData = {
-  budget: money,
-  expenses: {},
-  optionalExpenses: {},
-  income: [],
-  timeData: time,
-  savings: false
-};
+// !!!!!!!!! программа учета через alert
 
-for (let i = 0; i < 2; i++) {
-    let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
-        b = prompt('Во сколько это обойдется?', '');
+// let money = +prompt('Ваш бюджет на месяц?', ''),
+//     time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+// let appData = {
+//   budget: money,
+//   expenses: {},
+//   optionalExpenses: {},
+//   income: [],
+//   timeData: time,
+//   savings: false
+// };
+
+// for (let i = 0; i < 2; i++) {
+//     let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+//         b = prompt('Во сколько это обойдется?', '');
     
-    if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
-       && a!= '' && b!= '' && a.length < 50) {
-       console.log('done');
-       appData.expenses[a] = b;
-  }  else {
-          // прописать условие
-  };
-}
+//     if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
+//        && a!= '' && b!= '' && a.length < 50) {
+//        console.log('done');
+//        appData.expenses[a] = b;
+//   }  else {
+//           // прописать условие
+//   };
+// }
 
-appData.moneyPerDay = appData.budget / 30;
+// appData.moneyPerDay = appData.budget / 30;
 
-alert('Ежедневный бюджет: ' + appData.moneyPerDay);
+// alert('Ежедневный бюджет: ' + appData.moneyPerDay);
 
-if(appData.moneyPerDay < 100) {
-    console.log('Минимальный уровень дохода');
-}  else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-    console.log('Средний уровень дохода');
-}  else if (appData.moneyPerDay > 2000) {
-    console.log('Высокий уровень дохода');
-}  else {
-    console.log('Произошла ошибка')
-}
+// if(appData.moneyPerDay < 100) {
+//     console.log('Минимальный уровень дохода');
+// }  else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+//     console.log('Средний уровень дохода');
+// }  else if (appData.moneyPerDay > 2000) {
+//     console.log('Высокий уровень дохода');
+// }  else {
+//     console.log('Произошла ошибка')
+// }
+
+// !!!!!!!!! программа учета через alert конец
+
+// let userName = prompt('Who are you hommie?', '');
+
+// if(userName === 'Admin') {
+
+//     let pass = prompt('Password?', '');
+
+//     if (pass === 'Main') {
+//         alert('Welcome home captain!');    
+//     }   else if (pass === '' || pass === null){
+//         alert('Canceled');
+//     }    else {
+//             alert('Wrong password');
+//         }
+
+//     } else if (userName === '' || userName === null) {
+//       alert('Canceled');
+//     } else {
+//       alert('I dont know you');
+//     }   
+
+
+// let num = 20;
+// function showFirstMessage(text) {
+//     alert(text);
+//     let num = 10; 
+//     console.log(num);
+// }
+
+
+// showFirstMessage('Default text');
+// console.log(num);
+
+// function calc(a,b) {
+//     return (a + b);
+
+// }
+
+// console.log(calc(2,8)) ;
+
+let calc = (a,b) => a+b 
+
+console.log(calc(3,8));
+
+
+let str = 'test';
+console.log(str.length);
+
+console.log(str.toLocaleUpperCase());
+console.log(str.toLocaleLowerCase());
