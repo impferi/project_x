ef68aa0328dc75dad0cd12de12247d3c12acdcd4
Roman <tajmur@gmail.com>
2024-04-12 21:19:29 +0700

    new

--- project_one/script.js
+++ project_one/script.js
@@ -173,13 +173,148 @@
 
 // console.log(calc(2,8)) ;
 
-let calc = (a,b) => a+b 
+// let calc = (a,b) => a+b 
 
-console.log(calc(3,8));
+// console.log(calc(3,8));
+
+// let str = 'test';
+// console.log(str.length);
+
+// console.log(str.toLocaleUpperCase());
+// console.log(str.toLocaleLowerCase());
+
+// const fnWithError = () => {
+//     throw new Error('Some Error');
+// }
+
+// try {
+//     fnWithError();
+// } catch (error) {
+//     console.error(error);
+//     console.log(error.message);
+// }
 
 
-let str = 'test';
-console.log(str.length);
+// console.log('Continue...')
 
-console.log(str.toLocaleUpperCase());
-console.log(str.toLocaleLowerCase());
+// let myArray = [1, 2, 3];
+// myArray.forEach(m => console.log(m * 2))
+
+// console.log(myArray)
+
+// const leagueChampions = {
+//     Inter: 'Milan',
+//     Juvenstus: 'Torino',
+//     Real: 'Madrid', 
+//     Barsa: 'Barcelona'
+// }
+
+// let {Inter, Juvenstus} = leagueChampions;
+
+// console.log(Inter)
+
+// const leagueMassive = ['Milan', 'Inter', 'Lazio']
+
+// let [clubOne, clubTwo, ClubTwo] = leagueMassive;
+// console.log(clubOne)
+
+// const userProfile = {
+//     name : 'Alex',
+//     commentsQty: 23,
+//     hasSignAgreement: false,
+// }
+
+// const userInfo = ({ name, commentsQty }) => {
+//     if (!commentsQty) {
+//         return `User ${name} has no comments`    
+// }
+//         return `User ${name} has ${commentsQty} comments`
+// } 
+
+// console.log(userInfo(userProfile)) ; 
+
+// const person = {
+//     age: 20,
+//     name: 'Luther'
+// }
+
+// if (!person.name) {
+//     console.log('Name is empty')
+// } else console.log('Hello Luther!')
+
+// let aBle = 88 ;
+
+// if (aBle < 90) {
+//     console.log('Less')
+// } else if (aBle === 90) {
+//     console.log('Correct')
+// } else {
+//     console.log('Biggest')
+// }
+
+// switch (101 + 100) {
+//     case 150:
+//         console.log('Not correct')
+//       break
+//     case 200:
+//         console.log('Correct,God damn!')
+//       break
+//     default:
+//         console.log('Try again bro')
+// }   
+
+// let weather = 29
+// console.log(
+//     weather >= 30
+//     ? 'У вас очень жарко!'
+//     : -weather
+// );
+
+
+// for (let fi = 0; fi < 10; fi++) {
+//     console.log(fi)
+// }
+
+
+// const atpRating = {
+//     top1: 'Djokovic',
+//     top2: 'Sinner',
+//     top3: 'Alcaraz',
+//     top4: 'Medvedev',
+//     top5: 'Nadal'
+// }
+
+// Object.keys(atpRating).forEach(kluch => {
+//     console.log(kluch, atpRating[kluch])
+// });
+
+// Object.values(atpRating).forEach(znachenie => {
+//     console.log(znachenie)
+// });
+
+
+class Comment {
+    constructor(sho) {
+        this.text = sho
+        this.votesQty = 0
+    }
+
+    upvote() {
+        this.votesQty += 1
+    }
+
+    static mergeComments(first, second) {
+        return `${first} ${second}`
+    }
+}
+
+const firstComment = new Comment ('First Comment')
+console.log(firstComment)
+
+for (let index = 0; index < 5 ; index++) {
+    firstComment.upvote();
+}
+
+
+ console.log(firstComment.votesQty)
+ console.log(firstComment.text)
